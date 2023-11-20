16146cbb75d08d2999e2b9e6225d9b1e6c85d4dc
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-11-20 21:28:41 +0530

    Create a counter button.

---
+++ Counter (Same as default UI of react : vite)/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>State in react</h1>
+      <h3>Counter button</h3>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
