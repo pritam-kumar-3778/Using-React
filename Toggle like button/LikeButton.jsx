cc6ae7213ee80fa0349dd832ca4ae0c3e07668c0
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-11-21 00:20:47 +0530

    Add a functionality (clicks count)
    Number of clicks count.

--- Toggle like button/LikeButton.jsx
+++ Toggle like button/LikeButton.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 
 const LikeButton = () => {
   let [isLiked, setIsLiked] = useState(false);
+  let [click, setClick] = useState(0);
 
   let toggleLike = () => {
     setIsLiked(!isLiked);
+    setClick(click+1);
   };
 
   let likeStyle= {color:"red"};
 
   return (
     <div>
+      <p>clicks = {click}</p>
       <p onClick={toggleLike}>
         {isLiked ? (
           <i className="fa-solid fa-heart" style={likeStyle}></i>
