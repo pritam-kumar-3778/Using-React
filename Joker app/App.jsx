4afc0b78571b8a625b8aa1c4c9a55e869a61c210
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-11-24 23:32:30 +0530

    Update App.jsx

--- Joker app/App.jsx
+++ Joker app/App.jsx
@@ -1,16 +1,9 @@
-
-import CommentsForm from "./CommentsForm"
-import Counter from "./Counter"
-import Form from "./Form"
 import Joker from "./Joker"
 
 function App() {
 
   return (
     <>
-      {/* <Form/> */}
-      {/* <CommentsForm/> */}
-     {/* <Counter/> */}
      <Joker/>
     </>
   )
