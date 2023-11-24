3602e1ab8b863912c9b34df665ffeeeec0921578
Pritam Kumar Patel <127990174+pritam-kumar-3778@users.noreply.github.com>
2023-11-24 23:31:46 +0530

    App.jsx file push of Joker app

---
+++ Joker app/App.jsx
@@ -0,0 +1,19 @@
+
+import CommentsForm from "./CommentsForm"
+import Counter from "./Counter"
+import Form from "./Form"
+import Joker from "./Joker"
+
+function App() {
+
+  return (
+    <>
+      {/* <Form/> */}
+      {/* <CommentsForm/> */}
+     {/* <Counter/> */}
+     <Joker/>
+    </>
+  )
+}
+
+export default App
